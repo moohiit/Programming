76c34c880c808e3ee0953048a260917ebf57ab80
moohiit <pmohit645@gmail.com>
2023-09-12 16:26:19 +0530

    New Repository Created

---
+++ Coursera/my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import Heading from './Heading';
+function App() { 
+  return ( 
+    <div className="App"> 
+      <Heading  firstname="Mohit"/>
+      <Heading  firstname="Arun"/>
+
+    </div> 
+  ); 
+}
+ 
+export default App;
